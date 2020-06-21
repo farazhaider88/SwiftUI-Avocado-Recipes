eab028491fa545549c919b8d344de55612d360f7
farazhaider88 <farazhaider88@yahoo.com>
2020-06-21 15:19:54 +0500

    Added Header View With ScrollView

--- Avocado Recipes/Avocado Recipes/View/ContentView.swift
+++ Avocado Recipes/Avocado Recipes/View/ContentView.swift
@@ -10,7 +10,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Avocado Recipies")
+        ScrollView(.vertical,showsIndicators: false){
+            VStack(alignment: .center, spacing: 20){
+                //MARK: - Header
+                ScrollView(.horizontal,showsIndicators: false){
+                    HStack{
+                        HeaderView()
+                    }
+                }
+                
+                //MARK: - Footer
+                VStack(alignment: .center, spacing: 20){
+                    Text("All About Avocados").font(.system(.title,design:.serif)).fontWeight(.bold).foregroundColor(Color("ColorGreenAdaptive")).padding(8)
+                    Text("Everything you wanted to know about Avocado but afraid to ask.").font(.system(.body, design: .serif)).multilineTextAlignment(.center).foregroundColor(.gray)
+                }.frame(maxWidth:640).padding().padding(.bottom,85)
+            }
+        }.edgesIgnoringSafeArea(.all).padding(0)
     }
 }
 
